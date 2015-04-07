d8caa9eded9534b93d7bc40a1eee1c20b58743df
Justin Myers <jmyers@ap.org>
2015-04-07 18:05:26 -0400

    Start defining Dataset accessor API
    Need to figure out what it returns, but still useful to think through.

--- lib/ailurus/client.rb
+++ lib/ailurus/client.rb
@@ -1,3 +1,8 @@
+require "json"
+require "net/http"
+
+require "ailurus/utils"
+
 module Ailurus
   # Public: Initialize a client object through which to interact with a PANDA
   # server.
@@ -14,6 +19,20 @@ module Ailurus
       config.each do |key, value|
         instance_variable_set("@#{key}", value)
       end
+    end
+
+    def dataset(slug)
+      dataset_url = URI.join(
+        Ailurus::Utils::get_absolute_uri(@domain),
+        "/api/1.0/dataset/#{slug}/")  # FIXME: Prohibit `..` et al.
+      dataset_url.query = URI.encode_www_form({
+        :format => "json",
+        :email => @email,
+        :api_key => @api_key
+      })
+
+      res = Net::HTTP.get_response(dataset_url)
+      return JSON.parse(res.body)  # TODO: Do more with this.
     end
   end
 end
--- spec/ailurus/client_spec.rb
+++ spec/ailurus/client_spec.rb
@@ -14,4 +14,22 @@ describe Ailurus::Client do
     expect(client.domain).to eq("domain name")
     expect(client.email).to eq("email address")
   end
+
+  it "adds authentication parameters to requests" do
+    stub_request(:any, /panda\.example\.com/).to_return(:body => "{}")
+
+    client = Ailurus::Client.new(
+      :api_key => "API_KEY_HERE",
+      :domain => "panda.example.com",
+      :email => "user@example.com"
+    )
+    example_dataset = client.dataset("example")
+
+    expected_url = "http://panda.example.com/api/1.0/dataset/example/"
+    expect(WebMock).to have_requested(:get, expected_url).with(:query => {
+      "format" => "json",
+      "email" => "user@example.com",
+      "api_key" => "API_KEY_HERE"
+    })
+  end
 end
