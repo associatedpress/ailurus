ede26059503f125b00d06cd1cb91714fdb117124
Justin Myers <jmyers@ap.org>
2015-04-07 15:58:13 -0400

    Clean up test syntax a bit
    Part of me appreciates this, and part of me thinks there's too much
    magic. (The eternal struggle.)

--- spec/ailurus/client_spec.rb
+++ spec/ailurus/client_spec.rb
@@ -4,11 +4,11 @@ require "ailurus/client"
 
 describe Ailurus::Client do
   it "initializes properly" do
-    client = Ailurus::Client.new({
+    client = Ailurus::Client.new(
       :api_key => "api key",
       :domain => "domain name",
       :email => "email address"
-    })
+    )
 
     expect(client.api_key).to eq("api key")
     expect(client.domain).to eq("domain name")
